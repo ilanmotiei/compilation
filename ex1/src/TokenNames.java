3139c62f180a286596951be721f2537c78cef7d1
‪Ilan Motiei‬‏ <85764577+ilanmotiei@users.noreply.github.com>
2021-10-24 11:57:58 +0300

    Create TokenNames.java

---
+++ ex1/src/TokenNames.java
@@ -0,0 +1,34 @@
+
+public interface TokenNames {
+	
+	public static final int EOF = 0;
+	public static final int INT = 1;
+	public static final int STRING = 2;
+	public static final int ID = 3;
+	public static final int LPAREN = 4;
+	public static final int RPAREN = 5;
+	public static final int LBRACK = 6;
+	public static final int RBRACK = 7;
+	public static final int LBRACE = 8;
+	public static final int RBRACE = 9;
+	public static final int NIL = 10;
+	public static final int PLUS = 11;
+	public static final int MINUS = 12;
+	public static final int TIMES = 13;
+	public static final int DIVIDE = 14;
+	public static final int COMMA = 15;
+	public static final int DOT = 16;
+	public static final int SEMICOLON = 17;
+	public static final int ASSIGN = 18;
+	public static final int EQ = 19;
+	public static final int LT = 20;
+	public static final int GT = 21;
+	public static final int ARRAY = 22;
+	public static final int CLASS = 23;
+	public static final int EXTENDS = 24;
+	public static final int RETURN = 25;
+	public static final int WHILE = 26;
+	public static final int IF = 27;
+	public static final int NEW = 28;	
+	
+}
